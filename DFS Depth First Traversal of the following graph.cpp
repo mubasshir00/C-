4987392c0f32aeb8ff69009fa085344075e62b3d
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2018-11-03 19:22:27 +0600

    Update DFS Depth First Traversal of the following graph.cpp

--- DFS Depth First Traversal of the following graph.cpp
+++ DFS Depth First Traversal of the following graph.cpp
@@ -5,19 +5,19 @@ vector<int >graph[maxSize];
 void DFSUtil(int x ,vector<int>graph[],vector<bool>&visited)
 {
 
-    visited[x]=true;
+    visited[x]=true; //  visited then print x  
     cout<<x<<" ";
     for(int i=0;i<graph[x].size();i++)
-        if(visited[graph[x][i]]==false)
-            DFSUtil(graph[x][i],graph,visited);
+        if(visited[graph[x][i]]==false)   
+            DFSUtil(graph[x][i],graph,visited);  //if nor visited then repeat DFSUtil
     }
 
 DFS(vector<int>graph[],int V)
 {
     vector<bool>visited(V,false);
     for(int x=0;x<V;x++)
-        if(visited[x]==false)
-            DFSUtil(2,graph,visited);
+        if(visited[x]==false) // not visited
+            DFSUtil(2,graph,visited); // Traversal Start from 2 
 }
 int main()
 {
