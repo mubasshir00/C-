e5ece5b4ad3d475b1e9ee3bbaf054034a673ec64
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2019-05-22 17:15:26 +0600

    Update and rename Binary-Exponentiation.cpp to Binary-Exponentiation_POWER_OF_NUMBER_1st_three_digit.cpp

---
+++ Binary-Exponentiation_POWER_OF_NUMBER_1st_three_digit.cpp
@@ -0,0 +1,53 @@
+long long binpow(long long a, long long b) {
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        return res * res;
+}
+
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int firstThree(long long x,long long y)
+{
+    double z = y*log10(x);
+    long long temp = z ;
+    z=z-temp;
+    z=(pow(10,z));
+    z=z*100;
+    return (int)z;
+}
+
+int main()
+{
+    long long tc ;
+
+
+
+    cin>>tc;
+
+                while(tc--){
+        long long c,d,e;
+        cin>>c>>d;
+        //string str = std::to_string(c,d);
+        //cout<<binpow(c,d)<<endl;
+        //cout<<v[v.size()-1]<<endl;
+       // v.clear();
+       printf("%03d...%03d\n",firstThree(c,d),binpow(c,d));
+       //cout<<firstThree(c,d)<<"..."<<binpow(c,d)<<endl;
+                }
+
+
+
+    return 0 ;
+}
