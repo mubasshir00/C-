90969d5a4681e554c4e87b0ae5f058b88988c825
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2019-05-25 02:24:08 +0600

    Update Binary-Exponentiation_POWER_OF_NUMBER_1st_three_digit.cpp

--- Binary-Exponentiation_POWER_OF_NUMBER_1st_three_digit.cpp
+++ Binary-Exponentiation_POWER_OF_NUMBER_1st_three_digit.cpp
@@ -8,6 +8,19 @@ long long binpow(long long a, long long b) {
         return res * res;
 }
 
+long long m =10;
+long long binpow(long long a, long long b) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
 long long binpow(long long a, long long b) {
     long long res = 1;
     while (b > 0) {
