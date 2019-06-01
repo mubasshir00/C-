6bd9d2d20ba6c9d119891190cb50665a6bbb9e81
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2019-06-02 02:01:50 +0600

    Update Fibonacci Numbers.cpp

--- Fibonacci Numbers.cpp
+++ Fibonacci Numbers.cpp
@@ -30,7 +30,7 @@ int fib(int n)
     if(f[n])return f[n];
     int k = (n & 1) ? (n+1)/2 : n/2;
     f[n] = (n & 1) ? (fib(k)*fib(k) + fib(k-1)*fib(k-1) )
-    : (2*fib(k-1) + fib(k)*fib(k));
+    : (2*fib(k-1) + fib(k))*fib(k);
     return f[n];
 }
 
